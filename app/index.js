0408d7ab369e8dd40d30c428bc378c5e2a127b48
dev-abolfazlabbasi82 <engineerabolfazl82@gmail.com>
2025-01-11 23:17:59 +0330

    final

---
+++ app/index.js
@@ -0,0 +1,12 @@
+// const link = document.querySelectorAll(".navigation__link");
+// const btn = document.querySelector(".navigation-btn");
+// const bg = document.querySelector(".navigation-background");
+// const nav = document.querySelector(".navigation-nav");
+
+// for (let i = 0; i < link.length; i++) {
+//   link[i].addEventListener("click", () => {
+//     nav.style.width = "0px";
+//     nav.style.display = "none";
+//     bg.style.transform = 'scale(0)';
+//   });
+// }
